f80490e7687f0ac21ee8b83cc4182c95e6531875
nhamill21 <tsoizhiv@ya.ru>
2021-02-18 01:42:46 +0500

    Добавлена проверка ссылок на фото.

--- src/main/resources/db/migration/V2.0__insert_ads.sql
+++ src/main/resources/db/migration/V2.0__insert_ads.sql
@@ -1,5 +1,3 @@
 INSERT INTO ad (ad_id, title, price, description, creation_date) VALUES (1, 'first', 21.42, 'hello', CURRENT_TIMESTAMP);
 INSERT INTO ad (ad_id, title, price) VALUES (2, 'second', 42.21);
 INSERT INTO ad (ad_id, title, price) VALUES (3, 'third', 0.69);
-INSERT INTO photos (photo_id, ad_id, photos) VALUES (1, 3, 'hello');
-INSERT INTO photos (photo_id, ad_id, photos) VALUES (2, 3, 'world');
---
+++ src/main/resources/db/migration/V2.1__insert_photos.sql
@@ -0,0 +1,3 @@
+INSERT INTO photos (photo_id, ad_id, photos) VALUES (1, 3, 'http://get-photo.com/photo1');
+INSERT INTO photos (photo_id, ad_id, photos) VALUES (2, 3, 'ftp://example.com');
+INSERT INTO photos (photo_id, ad_id, photos) VALUES (3, 1, 'file:///C:/dream/intern-in-avito');
